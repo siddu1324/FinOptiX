b4ef65691dd32099970b79519f5e58fea3cbd2f9
spq6r <spq6r@umsl.edu>
2024-04-27 02:48:05 -0500

    add backend server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
